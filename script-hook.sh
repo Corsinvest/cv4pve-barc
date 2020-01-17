92bd791e0dd34d7bccdfd85745d74f6bd17da265
Mephisto <mephisto@mephis.to>
2020-01-17 21:03:13 +0100

    Added fsfreeze support for all Clusternodes

--- script-hook.sh
+++ script-hook.sh
@@ -4,14 +4,14 @@
 # Process environment variables as received from and set by eve4pve-barc.
 
 hook() {
-    echo "EVE4PVE_BARC_PHASE:       $EVE4PVE_BARC_PHASE" 
-    echo "EVE4PVE_BARC_VMID:        $EVE4PVE_BARC_VMID" 
-    echo "EVE4PVE_BARC_PATH:        $EVE4PVE_BARC_PATH" 
-    echo "EVE4PVE_BARC_LABEL:       $EVE4PVE_BARC_LABEL" 
-    echo "EVE4PVE_BARC_KEEP:        $EVE4PVE_BARC_KEEP" 
-    echo "EVE4PVE_BARC_SNAP_NAME:   $EVE4PVE_BARC_SNAP_NAME" 
-    echo "EVE4PVE_BARC_BACKUP_FILE: $EVE4PVE_BARC_BACKUP_FILE" 
-
+#    echo "EVE4PVE_BARC_PHASE:       $EVE4PVE_BARC_PHASE" 
+#    echo "EVE4PVE_BARC_VMID:        $EVE4PVE_BARC_VMID" 
+#    echo "EVE4PVE_BARC_PATH:        $EVE4PVE_BARC_PATH" 
+#    echo "EVE4PVE_BARC_LABEL:       $EVE4PVE_BARC_LABEL" 
+#    echo "EVE4PVE_BARC_KEEP:        $EVE4PVE_BARC_KEEP" 
+#    echo "EVE4PVE_BARC_SNAP_NAME:   $EVE4PVE_BARC_SNAP_NAME" 
+#    echo "EVE4PVE_BARC_BACKUP_FILE: $EVE4PVE_BARC_BACKUP_FILE" 
+#    echo "EVE4PVE_BARC_HOST:        $EVE4PVE_BARC_HOST"
     case "$EVE4PVE_BARC_PHASE" in
         #clean job status
         clean-job-start);;
@@ -24,7 +24,10 @@ hook() {
         #create snapshot
         snap-create-pre);;
         snap-create-post);;
-        snap-create-abort);;
+        snap-create-abort) ssh root@$EVE4PVE_BARC_HOST /usr/sbin/qm guest cmd $EVE4PVE_BARC_VMID fsfreeze-thaw;;
+
+	snap-vm-pre) echo "snap-vm-pre -- " && ssh root@$EVE4PVE_BARC_HOST /usr/sbin/qm guest cmd $EVE4PVE_BARC_VMID fsfreeze-freeze;;  
+	snap-vm-post) echo "snap-vm-post -- " && ssh root@$EVE4PVE_BARC_HOST /usr/sbin/qm guest cmd $EVE4PVE_BARC_VMID fsfreeze-thaw;;
 
         #export
         export-pre);;
